0d1e8a732039eb7052d830cc80e49a22259ab3f3
Kavindu Ranganth <91959867+Rajapaksha99@users.noreply.github.com>
2024-12-03 16:40:22 +0530

    done explore food componets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import {Route, Routes} from 'react-router-dom'
+import Cart from './Pages/Cart/Cart'
+import Home from './Pages/Home/Home'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <NavBar/>
       <Routes>
-        <Route/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
     </div>
   )
---
+++ frontend/src/Context/StoreContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: (prev[itemId] || 0) + 1,
+    }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      const currentCount = prev[itemId] || 0;
+      if (currentCount > 1) {
+        return { ...prev, [itemId]: currentCount - 1 };
+      } else {
+        const { [itemId]: _, ...rest } = prev; // Remove item if count is 1
+        return rest;
+      }
+    });
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
