aa2cd378e882afe2e56845463413c99141e92942
Kavindu Ranganth <91959867+Rajapaksha99@users.noreply.github.com>
2024-12-03 20:48:23 +0530

    finish the all forntend part

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../Context/StoreContext' // Adjust the path as necessary
+import { useNavigate } from 'react-router-dom';
+
 
 const Cart = () => {
+
+  const {cartItems, food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+
+  const navigate = useNavigate();
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-item">
+        <div className="cart-item-title">
+          <p>Item</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id]>0){
+            return(
+              <>
+              <div className="cart-item-title cart-item-item">
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>${item.price*cartItems[item._id]}</p>
+                <p onClick={()=>removeFromCart(item._id)} className='cross'>*</p>
+              </div>
+              <hr />
+              
+              </>
+
+              
+            )
+
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className='cart-total-details'>
+              <p>Sub Total</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className='cart-total-details'>
+              <p>Delivery fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className='cart-total-details'>
+              <p>Total</p>
+              <p>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</p>
+            </div>
+            
+          </div>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+        </div>
+
+        <div className="cart-promocode">
+          <p>If you have a promocode,Enter it here</p>
+          <div className="cart-promocode-input">
+            <input type="text" placeholder='Enter code here' />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
